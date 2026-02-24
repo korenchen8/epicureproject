cabd901e7eb830316bdfd39d9e1a85c0bff61094
Koren Chen <koren.chen@moveo.co.il>
2026-02-24 15:50:27 +0200

    change header strings to local file and update icons to an array

---
+++ src/components/Header/Header.strings.ts
@@ -0,0 +1,4 @@
+export const HEADER_STRINGS = {
+  RESTAURANTS: "Restaurants",
+  CHEFS: "Chefs",
+};
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,22 @@
-import React,{useState} from "react";
+import { useState } from "react";
+import { HEADER_STRINGS } from "./Header.strings";
 import styles from "./Header.module.css";
 
 
 function Header() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    return(
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const icons = [
+    { src: "/assets/icons/search.svg", alt: "search" },
+    { src: "/assets/icons/login.svg", alt: "user" },
+    { src: "/assets/icons/bag.svg", alt: "shopping bag" },
+  ];
+  return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        
-   
+
+
         <div className={styles.leftSection}>
-          <button 
+          <button
             className={styles.hamburgerBtn}
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
@@ -18,37 +24,33 @@ function Header() {
           </button>
         </div>
 
-     
+
         <div className={styles.logoSection}>
           <img src="/assets/icons/epicure-logo.svg" alt="Epicure Logo" className={styles.logo} />
         </div>
 
-      
-        <div className={styles.rightSection}>
-            <button className={styles.iconBtn} type="button">
-          <img src="/assets/icons/search.svg" alt="search" className={styles.icon} />
-          </button>
-          <button className={styles.iconBtn} type="button">
-          <img src="/assets/icons/login.svg" alt="user" className={styles.icon} />
-          </button>
-          <button className={styles.iconBtn} type="button">
-          <img src="/assets/icons/bag.svg" alt="shopping bag" className={styles.icon} />
-          </button>
-        </div>
-      </div>
 
-   
-      {isMenuOpen && (
-        <div className={styles.mobileMenu}>
-          <button onClick={() => setIsMenuOpen(false)} className={styles.closeBtn}><img src="/assets/icons/x-icon.svg" alt="shopping bag"/></button>
-          <nav>
-            <ul>
-              <li>Restaurants</li>
-              <li>Chefs</li>
-            </ul>
-          </nav>
+        <div className={styles.rightSection}>
+          {icons.map((icon, index) => (
+            <button key={index} className={styles.iconBtn} type="button">
+              <img src={icon.src} alt={icon.alt} className={styles.icon} />
+            </button>
+          ))}
         </div>
-      )}
+
+
+        {isMenuOpen && (
+          <div className={styles.mobileMenu}>
+            <button onClick={() => setIsMenuOpen(false)} className={styles.closeBtn}><img src="/assets/icons/x-icon.svg" alt="shopping bag" /></button>
+            <nav>
+              <ul>
+                <li>{HEADER_STRINGS.RESTAURANTS}</li>
+                <li>{HEADER_STRINGS.CHEFS}</li>
+              </ul>
+            </nav>
+          </div>
+        )}
+      </div>
     </header>
   )
 }
