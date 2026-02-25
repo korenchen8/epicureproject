93db5d58cd409b26133ec44c3f49064364089a61
Koren Chen <koren.chen@moveo.co.il>
2026-02-25 16:38:02 +0200

    IconMeaning part completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import IconMeaning from './components/IconMeaning/IconMeaning';
 import PopularRestaurants from './components/PopularRestaurants/PopularRestaurants'
 import SignatureDish from './components/SignatureDish/SignatureDish';
 
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <PopularRestaurants />
       <SignatureDish />
+      <IconMeaning />
     </div>
   );
   
